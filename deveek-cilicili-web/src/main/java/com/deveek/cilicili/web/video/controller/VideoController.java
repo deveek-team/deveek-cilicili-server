92f892d943fe8715681d8ae5740b8fa54d7cf9c7
harvey <harveysuen0803@gmail.com>
2024-06-16 22:26:14 +0800

    feat(media): add api for getting video list

--- deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/controller/VideoController.java
+++ deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/controller/VideoController.java
@@ -1,13 +1,17 @@
 package com.deveek.cilicili.web.video.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.deveek.cilicili.web.common.video.model.dto.TopVideoPageDto;
+import com.deveek.cilicili.web.common.video.model.po.TopVideoPageVo;
 import com.deveek.cilicili.web.common.video.model.po.VideoPo;
+import com.deveek.cilicili.web.common.video.model.vo.TopVideoVo;
 import com.deveek.cilicili.web.video.service.VideoService;
 import com.deveek.common.constant.Result;
 import jakarta.annotation.Resource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +27,7 @@ public class VideoController {
     private VideoService videoService;
     
     @GetMapping("/api/v1/video/top/page")
-    public Result<Void> pageTopVideo(@RequestParam TopVideoPageDto topVideoPageDto) {
+    public Result<TopVideoPageVo> pageTopVideo(@ModelAttribute TopVideoPageDto topVideoPageDto) {
         Integer pageNo = topVideoPageDto.getPageNo();
         Integer pageSize = topVideoPageDto.getPageSize();
         
@@ -33,6 +37,19 @@ public class VideoController {
         
         List<VideoPo> videoPoList = videoPoPage.getRecords();
         
-        return Result.success();
+        long totalSize = videoPoPage.getTotal();
+        
+        List<TopVideoVo> topVideoVoList = videoPoList.stream()
+            .map(videoPo -> {
+                TopVideoVo topVideoVo = BeanUtil.copyProperties(videoPo, TopVideoVo.class);
+                return topVideoVo;
+            })
+            .toList();
+        
+        TopVideoPageVo topVideoPageVo = new TopVideoPageVo();
+        topVideoPageVo.setTopVideoVoList(topVideoVoList);
+        topVideoPageVo.setTotalSize(totalSize);
+        
+        return Result.success(topVideoPageVo);
     }
 }
