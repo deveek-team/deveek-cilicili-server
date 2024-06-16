08498a341e43743264eff94d0316324d319b78b0
Vocmi <2686782541@qq.com>
2024-06-16 15:21:27 +0800

    feat(user):Implement user registration

--- deveek-cilicili-web-common/src/main/java/com/deveek/cilicili/web/common/user/constant/UserResult.java
+++ deveek-cilicili-web-common/src/main/java/com/deveek/cilicili/web/common/user/constant/UserResult.java
@@ -13,4 +13,7 @@ public class UserResult {
     public static final Result PASSWORD_INVALID = new Result(2203, "password is invalid");
     
     public static final Result EMAIL_INVALID = new Result(2204, "email is invalid");
+
+    public static final Result CODE_INVALID = new Result(2205, "code is invalid");
+    public static final Result USER_EXISTS = new Result(2206, "user already exist");
 }
