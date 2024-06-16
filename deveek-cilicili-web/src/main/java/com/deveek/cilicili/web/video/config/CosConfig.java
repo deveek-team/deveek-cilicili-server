5e1f0e47fa5511b8331734343b3188e9c111d96f
suohm <suohm@123>
2024-06-16 15:12:56 +0800

    feat(global): add cos util

---
+++ deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/config/CosConfig.java
@@ -0,0 +1,45 @@
+package com.deveek.cilicili.web.video.config;
+
+import com.deveek.cilicili.web.video.constant.CosProperties;
+import com.deveek.cilicili.web.video.support.CosUtil;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.region.Region;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * author Shooter
+ */
+@Configuration
+@Slf4j
+public class CosConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public COSClient initCOSClient(CosProperties cosProperties){
+        COSCredentials cred = new BasicCOSCredentials(cosProperties.getSecretId(), cosProperties.getSecretKey());
+        Region region = new Region(cosProperties.getRegion());
+        ClientConfig clientConfig = new ClientConfig(region);
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        return new COSClient(cred, clientConfig);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CosUtil cosUtil(CosProperties cosProperties){
+        return new CosUtil(
+                cosProperties.getSecretId(),
+                cosProperties.getSecretKey(),
+                cosProperties.getRegion(),
+                cosProperties.getUrl(),
+                cosProperties.getBucketName(),
+                initCOSClient(cosProperties));
+    }
+
+}
---
+++ deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/constant/UploadConstant.java
@@ -0,0 +1,9 @@
+package com.deveek.cilicili.web.video.constant;
+
+/**
+ * @author Shooter
+ */
+public class UploadConstant {
+    public static final String UPLOAD_VIDEO_PATH = "/video/";
+    public static final String UPLOAD_IMAGE_PATH = "/image/";
+}
