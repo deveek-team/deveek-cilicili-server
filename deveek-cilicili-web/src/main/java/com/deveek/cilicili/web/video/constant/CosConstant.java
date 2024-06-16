b2ebac99e642cb5f671e2882eded9fe885800599
suohm <suohm@123>
2024-06-16 16:34:40 +0800

    style(video): rename UploadConstant to CosConstant

--- deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/constant/CosConstant.java
+++ deveek-cilicili-web/src/main/java/com/deveek/cilicili/web/video/constant/CosConstant.java
@@ -3,7 +3,7 @@ package com.deveek.cilicili.web.video.constant;
 /**
  * @author Shooter
  */
-public class UploadConstant {
+public class CosConstant {
     public static final String UPLOAD_VIDEO_PATH = "/video/";
     public static final String UPLOAD_IMAGE_PATH = "/image/";
 }
